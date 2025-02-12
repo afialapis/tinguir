44551cf3c069de684d25c4b6b915b64a71718551
afialapis <devel@afialapis.com>
2025-02-13 00:23:23 +0100

    upgrading deps

--- test/print.mjs
+++ test/print.mjs
@@ -1,12 +1,29 @@
 import * as codes from '../src/index.mjs'
 
+const COLUMNS = 7
+
+function _groupTexts(arr) {
+  return arr.reduce((acc, item, index) => {
+      if (index % COLUMNS === 0) acc.push([]);
+      acc[acc.length - 1].push(item);
+      return acc;
+  }, []);
+}
+
+
 console.log(`Testing ${codes.magenta_bold('tinguir')}...\n`)
 
 let texts= []
 
-Object.values(codes).map((c) => {
+Object.values(codes)
+  .filter((c) => c.name !== 'uncolor')
+  .map((c) => {
   texts.push(c(`${c.name}`))
 })
 
-console.log(texts.join(', '))
-console.log('')
+const groups = _groupTexts(texts)
+
+groups.map((group) => {
+  console.log(group.join('\t'))
+  console.log('')
+})
